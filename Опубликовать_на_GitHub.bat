a769999e2e00bd08574fd41ec44a556215939887
Renkiy <59077694+Renkiy@users.noreply.github.com>
2026-08-25 22:43:58 +0900

    Delete Опубликовать_на_GitHub.bat

--- Опубликовать_на_GitHub.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title GitHub Publisher
-cd /d "%~dp0"
-python tools\github_publisher.py
-if %errorlevel% neq 0 (
-    echo.
-    echo Python execution failed. Press any key to exit.
-    pause >nul
-)
